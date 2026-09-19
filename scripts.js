6f7543ba6942821379659ae901d7cd026521091e
Rafael Francisco <rafafrancisco03@gmail.com>
2026-09-19 16:36:56 -0300

    corrigindo js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-const URL = "https://dummyjson.com/recipes";
-
-async function chamarAPI() {
-    const resp = await fetch(URL);
-    const data = await resp.json();
-    console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
+ 
